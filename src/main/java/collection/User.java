36d66d5f2b56198c1bba387a4d2405f9a940a922
ignatown <ignatow.n@mail.ru>
2021-02-26 00:10:42 +0300

    2. Без переопределения equals и hashCode [#470017]

--- src/main/java/collection/User.java
+++ src/main/java/collection/User.java
@@ -1,9 +1,37 @@
 package collection;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
     private int children;
     Calendar birthday;
+
+    public User(String name, int children) {
+        this.name = name;
+        this.children = children;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", children=" + children +
+                ", birthday=" + birthday +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Robert", 2);
+        User user2 = new User("Robert", 2);
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, 1);
+        map.put(user2, 1);
+
+        map.entrySet().stream().forEach(System.out::println);
+
+    }
 }

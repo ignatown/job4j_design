02f1b6df289ede634c18a555e8fd95ffebb7dd1a
ignatown <ignatow.n@mail.ru>
2021-03-01 22:08:22 +0300

    8. Реализовать собственную структуру данных - HashMap [#470020]

---
+++ src/main/java/collection/SimpleMap.java
@@ -0,0 +1,116 @@
+package collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleMap<K, V> implements Iterable<K> {
+    private Node<K, V>[] tbl;
+    private int content;
+    private int size;
+    private int mdCnt = 0;
+
+    public boolean insert(K key, V value) {
+        Node<K, V> elm = new Node<>(hash(key), key, value);
+        int i = index(key);
+        if (tbl[i] != null) {
+            return false;
+        }
+        tbl[i] = elm;
+        if (size++ >= content * 0.75f) {
+            Node<K, V>[] oldTab = tbl;
+            content *= 1.5;
+            tbl = (Node<K, V>[]) new Node[content];
+            for (Node<K, V> elmnt : oldTab) {
+                if (elm != null) {
+                    tbl[elmnt.hash & (content - 1)] = elmnt;
+                }
+            }
+        }
+        mdCnt++;
+        return true;
+    }
+
+    public V get(K key) {
+        if (!hasKey(key)) {
+            throw new NoSuchElementException();
+        }
+        return tbl[index(key)].value;
+    }
+
+    public boolean delete(K key) {
+        if (!hasKey(key)) {
+            return false;
+        } else {
+        tbl[index(key)] = null;
+        mdCnt++;
+        return true;
+        }
+    }
+
+    private boolean hasKey(K key) {
+        int i = index(key);
+        if (!tbl[i].key.equals(key)) {
+            return false;
+        }
+        return true;
+    }
+
+    private int index(K key) {
+        return hash(key) & (content - 1);
+    }
+
+    private int hash(K key) {
+        if (key == null) {
+            return 0;
+        } else {
+            return key.hashCode();
+        }
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return new Iterator<>() {
+            private final int expMdCnt = mdCnt;
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (expMdCnt != mdCnt) {
+                    throw new ConcurrentModificationException();
+                }
+                if (index == content) {
+                    return false;
+                }
+                if (tbl[index] == null) {
+                    index++;
+                    return hasNext();
+                }
+                return true;
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return tbl[index++].key;
+            }
+        };
+    }
+
+    public int getCapacity() {
+        return content;
+    }
+
+    private static class Node<K, V> {
+        private final int hash;
+        private final K key;
+        private V value;
+        public Node(int hash, K key, V value) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+        }
+    }
+}

6dc9d2705a3309b1ca6bd4ced8163bb38f7abc69
ignatown <ignatow.n@mail.ru>
2021-03-12 16:30:02 +0300

    Почта. [#184458]

--- src/main/java/Mail/Mail.java
+++ src/main/java/Mail/Mail.java
@@ -3,47 +3,46 @@ package Mail;
 import java.util.*;
 
 public class Mail {
-    Map<String, Set<String>> base = new HashMap<>();
+
     Set<String> emails = new HashSet<>();
     Map<String, Set<String>> dubUsers = new HashMap<>();
     Set<String> emptyUsers = new HashSet<>();
     String tempUser;
 
-
-    public String findByEmail(String mail) {
-        for (String findUser:
-             base.keySet()) {
-            if (base.get(findUser).contains(mail)) {
+    public String findByEmail(String mail, Map<String, Set<String>> data) {
+        for (String findUser :
+                data.keySet()) {
+            if (data.get(findUser).contains(mail)) {
                 return findUser;
             }
         }
         return null;
     }
 
-    public Map<String, Set<String>> merge(Map<String, Set<String>> data){
-        for (String user:
-             data.keySet()) {
-            for (String userEmail:
-            data.get(user)) {
+    public Map<String, Set<String>> merge(Map<String, Set<String>> data) {
+        for (String user :
+                data.keySet()) {
+            for (String userEmail :
+                    data.get(user)) {
                 if (emails.contains(userEmail)) {
-                    tempUser = findByEmail(userEmail);
+                    tempUser = findByEmail(userEmail, data);
                     if (dubUsers.containsKey(tempUser)) {
                         dubUsers.get(tempUser).addAll(data.get(user));
                     } else {
-                   dubUsers.put(tempUser, data.get(user));
-                   emptyUsers.add(user);
+                        dubUsers.put(tempUser, data.get(user));
+                        emptyUsers.add(user);
                     }
                 } else {
                     emails.add(userEmail);
                 }
             }
         }
-        for (String user:
-             emptyUsers) {
+        for (String user :
+                emptyUsers) {
             data.remove(user);
         }
-        for (String user:
-             dubUsers.keySet()) {
+        for (String user :
+                dubUsers.keySet()) {
             data.put(user, dubUsers.get(user));
         }
         return data;
--- src/test/java/Mail/MailTest.java
+++ src/test/java/Mail/MailTest.java
@@ -3,29 +3,33 @@ package Mail;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
+
 public class MailTest {
 
     @Test
     public void when() {
+        Map<String, Set<String>> base = new HashMap<>();
         Mail mail = new Mail();
         HashSet<String> mails1 = new HashSet<>();
         HashSet<String> mails2 = new HashSet<>();
         HashSet<String> mails3 = new HashSet<>();
         mails1.add("1");
-        mail.base.put("user1", mails1);
         mails2.add("2");
-        mail.base.put("user2", mails2);
         mails3.add("1");
         mails3.add("3");
-        mail.base.put("user3", mails3);
-        mail.merge(mail.base);
-        assertEquals(mail.base.get("user1").toString(), "[1, 3]");
-        assertEquals(mail.base.get("user2").toString(), "[2]");
+        base.put("user1", mails1);
+        base.put("user2", mails2);
+        base.put("user3", mails3);
+        mail.merge(base);
+        assertEquals(mail.merge(base).get("user1").toString(), "[1, 3]");
+        assertEquals(mail.merge(base).get("user2").toString(), "[2]");
     }
 }

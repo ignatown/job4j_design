534740249beeca4ea0f0c6cfc8381d09d872024b
ignatown <ignatow.n@mail.ru>
2021-03-19 00:57:36 +0300

    0.1. FileOutputStream. [#470033]

--- src/main/java/put/multiplicationTable.java
+++ src/main/java/put/multiplicationTable.java
@@ -4,15 +4,15 @@ import java.io.FileOutputStream;
 
 public class multiplicationTable {
     public static void main(String[] args) {
-        String a = "";
+        StringBuilder a = new StringBuilder();
         for (int i = 1; i < 9; i++) {
             for (int j = 1; j < 9; j++) {
-                a+=i+"*"+j+"="+i*j+" ";
+                a.append(i + "*" + j + "=" + i * j+System.lineSeparator());
             }
-            a+="\n";
+
         }
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            out.write(a.getBytes());
+            out.write(a.toString().getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }

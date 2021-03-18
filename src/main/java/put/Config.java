f12a5a5325346b653e5f7e23eaa4e68b992b3199
ignatown <ignatow.n@mail.ru>
2021-03-19 01:54:03 +0300

    1. Читаем файл конфигурации [#470039]

---
+++ src/main/java/put/Config.java
@@ -0,0 +1,47 @@
+package put;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Config {
+    private final String path;
+    private  Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader in = new BufferedReader(new FileReader(this.path))) {
+            this.values = in.lines().filter(a -> !a.contains("//"))
+                    .filter(a -> !a.contains("//"))
+                    .filter(a -> !a.contains(System.lineSeparator()))
+                    .map(a -> a.split("="))
+                    .collect(Collectors.toMap(a -> a[0], a -> a[1]));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return this.values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
---
+++ src/test/java/put/ConfigTest.java
@@ -0,0 +1,31 @@
+package put;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("City"),
+                is("Moscow")
+        );
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("name"),
+                is("Petr Arsentev")
+        );
+    }
+}

59c3e7bda4fe5b09f591ef0e50b97b5abe36f866
ignatown <ignatow.n@mail.ru>
2021-03-28 18:14:39 +0300

    1. Бот [#470056]

--- src/main/java/put/EchoServer.java
+++ src/main/java/put/EchoServer.java
@@ -5,6 +5,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
+    public static String getContent(String str) {
+        return str.split(" ")[1].split("=")[1];
+    }
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -12,14 +17,22 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str;
-                    while (!(str = in.readLine()).isEmpty()) {
-                        System.out.println(str);
-                        if (str.contains("Bye")) {
-                            server.close();
+                    String str = in.readLine();
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    System.out.println("User: " + (getContent(str)));
+                        switch (getContent(str)) {
+                            case ("Hello"):
+                                out.write(("Hello too!\r\r\r\n").getBytes());
+                                break;
+                            case ("Exit"):
+                                out.write("Bye!\r\r\r\n".getBytes());
+                                out.flush();
+                                socket.close();
+                                break;
+                            default:
+                                out.write("What\r\n\r\n".getBytes());
+                                break;
                         }
-                    }
-                    out.write("HTTP/1.1 200 OK\r\n\"".getBytes());
                 }
             }
         }

f0bb890ff643565e2f88ce35ea047d6b19a5aac8
ignatown <ignatow.n@mail.ru>
2021-03-31 01:20:12 +0300

    5. Преобразование JSON в POJO. JsonObject [#470049]

--- src/main/java/serialization/Auto.java
+++ src/main/java/serialization/Auto.java
@@ -1,28 +1,15 @@
 package serialization;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import org.json.JSONObject;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Arrays;
 
-@XmlRootElement(name = "auto")
-@XmlAccessorType(XmlAccessType.FIELD)
 public class Auto {
-    @XmlAttribute
-    private boolean automaticTransmission;
-    @XmlAttribute
-    private int horsepower;
-    @XmlAttribute
-    private String model;
-    private Manufacturer manufacturer;
-    private String[] colors;
-
-    public Auto() {
-    }
+    private final boolean automaticTransmission;
+    private final int horsepower;
+    private final String model;
+    private final Manufacturer manufacturer;
+    private final String[] colors;
 
     public Auto(boolean automaticTransmission, int horsepower, String model, Manufacturer manufacturer, String[] colors) {
         this.automaticTransmission = automaticTransmission;
@@ -43,27 +30,39 @@ public class Auto {
                 '}';
     }
 
+    public boolean isAutomaticTransmission() {
+        return automaticTransmission;
+    }
+
+    public int getHorsepower() {
+        return horsepower;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public String[] getColors() {
+        return colors;
+    }
+
     public static void main(String[] args) {
         final Auto auto = new Auto(false, 102, "Accent",
                 new Manufacturer("Hyundai", "Korea"), new String[] {"Black", "White", "Cherry"});
 
-        final Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(auto));
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("automaticTransmission", auto.isAutomaticTransmission());
+        jsonObject.put("horsepower", auto.getHorsepower());
+        jsonObject.put("model", auto.getModel());
+        jsonObject.put("manufacturer", auto.getManufacturer());
+        jsonObject.put("colors", auto.getColors());
 
-        final String autoJson =
-                "{"
-                        + "\"automaticTransmission\":false,"
-                        + "\"horsepower\":102,"
-                        + "\"model\":\"Accent\","
-                        + "\"manufacturer\":"
-                        + "{"
-                        + "\"name\":\"Hyundai\","
-                        + "\"country\":\"Korea\""
-                        + "},"
-                        + "\"colors\":"
-                        + "[\"Black\",\"White\",\"Cherry\"]"
-                        + "}";
-        final Auto autoMod = gson.fromJson(autoJson, Auto.class);
-        System.out.println(autoMod);
+        System.out.println(jsonObject.toString());
+
+        System.out.println(new JSONObject(auto).toString());
     }
 }
--- src/main/java/serialization/Manufacturer.java
+++ src/main/java/serialization/Manufacturer.java
@@ -1,22 +1,23 @@
 package serialization;
 
-import com.sun.xml.txw2.annotation.XmlElement;
 
-import javax.xml.bind.annotation.XmlAttribute;
 
-@XmlElement(value = "manufacturer")
 public class Manufacturer {
-    @XmlAttribute
-    private String name;
-    @XmlAttribute
-    private String country;
+    private final String name;
+    private final String country;
 
-    public Manufacturer() {
-    }
 
     public Manufacturer(String name, String country) {
         this.name = name;
         this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
     }
 
     @Override
